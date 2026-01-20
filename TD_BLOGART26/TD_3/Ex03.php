e755f3b8058140d2a8570cc6cb6d6592ee26a153
MehdiAks <afantastik041@gmail.com>
2026-01-20 16:26:25 +0100

    exercice 3 & 4

--- TD_BLOGART26/TD_3/Ex03.php
+++ TD_BLOGART26/TD_3/Ex03.php
@@ -12,12 +12,21 @@
     <?php
     echo "<h1>Afankous Mehdi - Exercice 3</h1>";
     echo "<h2>Consigne :</h2>";
-    echo "<p> </p>";
+    echo "<p> Créez une méthode addition() qui prend 2 nombres entiers en paramètre (operande1 et operande2) 
+    et qui affiche une phrase sur le modèle suivant : 5 + 6 = 11. Le résultat du calcul doit être le bon
+    quels que soient les nombres en paramètre.</p>";
     echo "<h2>Exercice</h2>";
+
+
+    function addition($nombre1, $nombre2)
+    {
+        $resultat = $nombre1 + $nombre2;
+        echo "<p> $nombre1 + $nombre2 = $resultat </p>";
+    }
+    addition(12, 13)
     ?>
 
-
-<?php include "../includes/footer.php"; ?>
+    <?php include "../includes/footer.php"; ?>
 </body>
 
 </html>
--- TD_BLOGART26/TD_3/Ex04.php
+++ TD_BLOGART26/TD_3/Ex04.php
@@ -12,12 +12,23 @@
     <?php
     echo "<h1>Afankous Mehdi - Exercice 4</h1>";
     echo "<h2>Consigne :</h2>";
-    echo "<p> </p>";
+    echo "<p>Créez une méthode multiplication() qui prend 2 nombres entiers en paramètre (operande1 et operande2)
+    et qui affiche une phrase sur le modèle suivant : 5 * 6 = 30.
+    Attention, cette fois-ci les paramètres doivent avoir une valeur par défaut s’ils ne 
+    sont pas renseignés (par exemple 5 et 10).
+ </p>";
     echo "<h2>Exercice</h2>";
+
+    function multiplication($nombre1 = 5, $nombre2 = 10)
+    {
+        $resultat = $nombre1 * $nombre2;
+        echo "<p> $nombre1 x $nombre2 = $resultat </p>";
+    }
+    multiplication(12, 13)
     ?>
 
 
-<?php include "../includes/footer.php"; ?>
+    <?php include "../includes/footer.php"; ?>
 </body>
 
 </html>
