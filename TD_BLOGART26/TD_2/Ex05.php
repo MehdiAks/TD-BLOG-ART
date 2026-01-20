9c4fbee73576ba2648e49f517f544e693b07dddd
MehdiAks <afantastik041@gmail.com>
2026-01-20 15:04:43 +0100

    TD 2 terminé

--- TD_BLOGART26/TD_2/Ex05.php
+++ TD_BLOGART26/TD_2/Ex05.php
@@ -12,9 +12,49 @@
     <?php
     echo "<h1>Afankous Mehdi - Exercice 5</h1>";
     echo "<h2>Consigne</h2>";
-    echo "<p> </p>";
+    echo "<p> Reprenez le tableau précédent. Affichez cette fois les valeurs dans un tableau HTML où chaque 
+    ligne correspond à une voiture sur le modèle de cases suivantes :
+    Marque | Couleur | Nombre de places | Quantité </p>";
     echo "<h2>Exercice</h2>";
+
+
+    $voitures = array(
+        array("Volvo", "rouge", "4 places", 3),
+        array("Renault", "rouge", "4 places", 3),
+        array("BMW", "blanche", "coupé", 4),
+        array("Saab", "bleue", "4 places", 4),
+        array("Renault", "blanche", "4 places", 3),
+        array("Ford", "rouge", "4 places", 5),
+        array("Renault", "rouge", "4 places", 2),
+        array("Land Rover", "verte", "8 places", 2),
+        array("Ford", "verte", "2 places", 10)
+    );
+    echo "<table>";
+
+    /* Ligne d'en-tête */
+    echo "<tr>";
+    echo "<th>Marque</th>";
+    echo "<th>Couleur</th>";
+    echo "<th>Nombre de places</th>";
+    echo "<th>Quantité</th>";
+    echo "</tr>";
+
+    /* Parcours du tableau des voitures */
+    for ($i = 0; $i < count($voitures); $i++) {
+
+        echo "<tr>";
+
+        for ($j = 0; $j < count($voitures[$i]); $j++) {
+            echo "<td>" . $voitures[$i][$j] . "</td>";
+        }
+
+        echo "</tr>";
+    }
+
+    echo "</table>";
+
     ?>
+
 </body>
 
 </html>
--- TD_BLOGART26/TD_2/Ex06.php
+++ TD_BLOGART26/TD_2/Ex06.php
@@ -12,9 +12,33 @@
     <?php
     echo "<h1>Afankous Mehdi - Exercice 6</h1>";
     echo "<h2>Consigne</h2>";
-    echo "<p> </p>";
+    echo "<p> Dans cet exercice, il s’agit de confirmer votre aisance dans la manipulation des boucles. 
+    Vous avez le choix dans l’utilisation des boucles : for, while, do… while….  
+    Même si l’une d’elles est plus naturelle que les autres.
+    L’objectif est d’afficher un X à l'écran dont la taille sera définie dans une variable. 
+    Vous aurez besoin de 2 boucles pour y parvenir.
+    Dans l’exemple ci-dessous, l’affichage du X fonctionne pour toutes les tailles – paire et impaire) :  </p>";
     echo "<h2>Exercice</h2>";
-    ?>
+
+$taille = 9; 
+echo "<div class='card'>";
+echo "<p class='small'>Taille : $taille</p>";
+echo "<pre>";
+
+for ($i = 0; $i < $taille; $i++) {
+    for ($j = 0; $j < $taille; $j++) {
+        if ($j == $i || $j == ($taille - 1 - $i)) {
+            echo "X";
+        } else {
+            echo " ";
+        }
+    }
+    echo "\n";
+}
+
+echo "</pre>";
+echo "</div>";
+?>
 </body>
 
 </html>
